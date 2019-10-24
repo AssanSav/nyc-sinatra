dd1404ba0bb227e7a98281cffb7a668268868afc
Assane Savadogo <assane.savadogo81@gmail.com>
2019-10-24 12:36:56 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,13 +1,13 @@
 class FiguresController < ApplicationController
   
   get '/figures/new' do 
-    @titles = Title.all
-    @landmarks = Landmark.all
+    # @titles = Title.all
+    # @landmarks = Landmark.all
     erb :'/figures/new'
   end
   
   get '/figures/:id' do 
-    @figure= Figure.find_by(params[:id])
+    @figure = Figure.find_by(params[:id])
     erb :'/figures/show'
   end
   
@@ -17,16 +17,20 @@ class FiguresController < ApplicationController
     end
     if !params[:title]["name"].empty? 
       title = Title.create(name: params[:title]["name"])
-      @figure.title_ids << title
-      @figure.title_ids = params["figure"][:title_ids]
-      @figure.save
+      @figure.titles << title
     end
+    params["figure"][:title_ids].each do |id|
+      new_id = Title.find_by_id(id)
+      @figure.titles << new_id
+    end
+
     if !params[:landmark]["name"].empty? && !params[:landmark]["year"].empty?
       landmark = Landmark.create(name: params[:landmark]["name"], year_completed: params[:landmark]["year"])
       @figure.landmarks << landmark
     end
     @figure.save
     binding.pry
+    redirect to "/figures/#{@figure.id}"
   end
   
 end
