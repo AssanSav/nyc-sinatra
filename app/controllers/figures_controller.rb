6af8fab749591a2a9e445cb8e6a8482940ee89ae
Assane Savadogo <assane.savadogo81@gmail.com>
2019-10-24 13:09:30 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,13 +1,15 @@
 class FiguresController < ApplicationController
   
   get '/figures/new' do 
-    # @titles = Title.all
-    # @landmarks = Landmark.all
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :'/figures/new'
   end
   
   get '/figures/:id' do 
+    @figures = Figure.all
     @figure = Figure.find_by(params[:id])
+    binding.pry
     erb :'/figures/show'
   end
   
@@ -15,6 +17,7 @@ class FiguresController < ApplicationController
     if !params[:figure]["name"].empty?
       @figure = Figure.create(name: params[:figure]["name"])
     end
+
     if !params[:title]["name"].empty? 
       title = Title.create(name: params[:title]["name"])
       @figure.titles << title
@@ -23,13 +26,17 @@ class FiguresController < ApplicationController
       new_id = Title.find_by_id(id)
       @figure.titles << new_id
     end
-
+    
     if !params[:landmark]["name"].empty? && !params[:landmark]["year"].empty?
       landmark = Landmark.create(name: params[:landmark]["name"], year_completed: params[:landmark]["year"])
       @figure.landmarks << landmark
     end
+    
+    params[:figure][:landmark_ids].each do |id|
+      new_id = Landmark.find_by_id(id)
+      @figure.landmark_ids << new_id
+    end
     @figure.save
-    binding.pry
     redirect to "/figures/#{@figure.id}"
   end
   
