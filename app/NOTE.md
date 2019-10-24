0daf35fd237794a58ec525097493769aace24d89
Assane Savadogo <assane.savadogo81@gmail.com>
2019-10-24 10:50:55 -0400

    Done.

---
+++ app/NOTE.md
@@ -0,0 +1,18 @@
+{"figure"=>{"name"=>""}, "title"=>{"name"=>""}, "landmark"=>{"name"=>"Green Market"}, "year"=>"1998"}
+
+
+FiguresController
+  allows you to view form to create a new figure
+
+  allows you to create a new figure with a title (FAILED - 1)
+
+  allows you to create a new figure with a landmark (FAILED - 2)
+
+  allows you to create a new figure with a new title (FAILED - 3)
+  
+  allows you to create a new figure with a new landmark (FAILED - 4)
+  creates checkboxes for all the landmarks and titles created on the Figures new page
+  allows you to list all figures (FAILED - 5)
+  allows you to see a single Figure (FAILED - 6)
+  allows you to view form to edit a single figure (FAILED - 7)
+  allows you to edit a single figure (FAILED - 8)
--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,32 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures/new' do 
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+  
+  get '/figures/:id' do 
+    @figure= Figure.find_by(params[:id])
+    erb :'/figures/show'
+  end
+  
+  post '/figures' do 
+    if !params[:figure]["name"].empty?
+      @figure = Figure.create(name: params[:figure]["name"])
+    end
+    if !params[:title]["name"].empty? 
+      title = Title.create(name: params[:title]["name"])
+      @figure.title_ids << title
+      @figure.title_ids = params["figure"][:title_ids]
+      @figure.save
+    end
+    if !params[:landmark]["name"].empty? && !params[:landmark]["year"].empty?
+      landmark = Landmark.create(name: params[:landmark]["name"], year_completed: params[:landmark]["year"])
+      @figure.landmarks << landmark
+    end
+    @figure.save
+    binding.pry
+  end
+  
 end
